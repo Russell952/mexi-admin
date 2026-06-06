9b05b865975e90642d0144d27814a5d1160f201a
Russell Eshiet <russelleshiet107@gmail.com>
2026-06-06 10:55:34 +0100

    built admin page

--- admin/AdminLayout.jsx
+++ admin/AdminLayout.jsx
@@ -3,20 +3,12 @@ import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
 function AdminLayout({ children }) {
-console.log("AdminLayout rendered");
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
       <Topbar setSidebarOpen={setSidebarOpen} />
     <div className="admin-layout">
-
-      {sidebarOpen && (
-        <div
-          className="sidebar-backdrop"
-          onClick={() => setSidebarOpen(false)}
-        />
-      )}
 
       <Sidebar
         sidebarOpen={sidebarOpen}
